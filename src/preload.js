db6c0d109f5506a2fcd259eed722efab64da6a80
kyim50 <kimanimac2005@gmail.com>
2025-10-30 12:11:14 -0500

    preload.js

--- src/preload.js
+++ src/preload.js
@@ -2,32 +2,40 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
-contextBridge.exposeInMainWorld('electronAPI', {
-  // User authentication
+contextBridge.exposeInMainWorld('api', {
+  // Login
   login: (credentials) => ipcRenderer.invoke('login', credentials),
+  
+  // Get user info
   getUser: () => ipcRenderer.invoke('get-user'),
   
   // Voice controls
   toggleMute: () => ipcRenderer.invoke('toggle-mute'),
   toggleDeafen: () => ipcRenderer.invoke('toggle-deafen'),
   leaveVoice: () => ipcRenderer.invoke('leave-voice'),
+  getVoiceStatus: () => ipcRenderer.invoke('get-voice-status'),
   
-  // Event listeners
-  onGameState: (callback) => {
-    ipcRenderer.on('game-state', (event, data) => callback(data));
-  },
-  onClientStatus: (callback) => {
-    ipcRenderer.on('client-status', (event, data) => callback(data));
-  },
-  onVoiceJoined: (callback) => {
-    ipcRenderer.on('voice-joined', (event, data) => callback(data));
-  },
-  onError: (callback) => {
-    ipcRenderer.on('error', (event, data) => callback(data));
+  // Event listeners (one-way from main to renderer)
+  on: (channel, callback) => {
+    const validChannels = [
+      'league-status',
+      'game-status',
+      'voice-status',
+      'backend-status',
+      'mute-status',
+      'deafen-status',
+      'teammate-joined-voice',
+      'teammate-left-voice'
+    ];
+    
+    if (validChannels.includes(channel)) {
+      // Deliberately strip event as it includes `sender`
+      ipcRenderer.on(channel, (event, ...args) => callback(...args));
+    }
   },
   
-  // Remove listeners
-  removeListener: (channel) => {
-    ipcRenderer.removeAllListeners(channel);
+  // Remove event listener
+  removeListener: (channel, callback) => {
+    ipcRenderer.removeListener(channel, callback);
   }
 });
